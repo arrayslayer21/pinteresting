40136273a08b27abb24dbaff0e6aa800b8d16af2
jose reinoso <jleo042@gmail.com>
2014-06-08 10:04:51 -0400

    Update show view, add glyphicons, and change root path

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require masonry.pkgd.js
 //= require jquery.turbolinks
 //= require turbolinks
 //= require masonry/jquery.masonry
